8e47535a650e768d16f6c6f1c78df75dce0e58e4
la-aisha <sulamaish4738@gmail.com>
2024-03-08 13:51:15 +0100

    modif main

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.myColor),
         useMaterial3: true,
       ),
